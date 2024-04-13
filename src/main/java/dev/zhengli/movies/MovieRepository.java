782d8ccd940daace5e4b12fdba694261be8baa3b
nickzheng87 <62922885+nickzheng87@users.noreply.github.com>
2024-04-14 00:12:57 +0800

    working on backend development

--- src/main/java/dev/zhengli/movies/MovieRepository.java
+++ src/main/java/dev/zhengli/movies/MovieRepository.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-//tesing for new proxy for windows
+//testing for new proxy for windows
 @Repository
 public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
 }
